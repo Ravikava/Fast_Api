a24310493d3d0e8e68b6dacf7fcdc226d9079858
Ravi Kava <kavaravi2@gmail.com>
2022-07-11 12:05:48 +0530

    Final Authentication Done.

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException,status
-from .. import schemas,database,models
+from fastapi.security import OAuth2PasswordRequestForm
+from .. import JWTtoken, schemas,database,models
 from..hashing import Hash
 from sqlalchemy.orm import Session
 
@@ -8,7 +9,7 @@ router = APIRouter(
 )
 
 @router.post('/login')
-def login(request:schemas.Login,db: Session = Depends(database.get_db)):
+def login(request:OAuth2PasswordRequestForm = Depends(),db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Invalid credentials")
@@ -16,4 +17,5 @@ def login(request:schemas.Login,db: Session = Depends(database.get_db)):
     if not Hash.verify(user.password,request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Incorrect Password")
     
-    return user
+    access_token = JWTtoken.create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
--- blog/schemas.py
+++ blog/schemas.py
@@ -43,4 +43,4 @@ class Token(BaseModel):
     token_type: str
 
 class TokenData(BaseModel):
-    username: Union[str, None] = None
+    email: Union[str, None] = None
